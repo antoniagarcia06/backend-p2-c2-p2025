de77bdb93cff18e065cccb012e3d762d4d7768e2
antoniagarcia06 <antonia.garcia06@inacapmail.cl>
2025-11-10 16:53:06 -0300

    .

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = 'django-insecure-9mdtlc$vi3wpe@*!xmey3r!r3encl1#by0!=r6%0#%fhkn@&&2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+      'backend-p2-c2-p2025-1.onrender.com', 
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
